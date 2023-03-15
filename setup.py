43dd3852d26801a9752c3313ec6754c3c21cf60d
Ben Baptist <sawham6@gmail.com>
2023-03-14 20:35:37 -0400

    added short description to pypi package

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='quickgpt',
-    version='0.0.1',
-    description='',
+    version='0.0.2',
+    description='A barebones library to make interacting with OpenAI\'s ChatGPT API much simpler.',
     long_description=open("README.rst", "r").read(),
     author='Ben Baptist',
     author_email='me@benbaptist.com',
